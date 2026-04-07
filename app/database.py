79c53bda88e75e8380fe34a3ebe16404d700a493
RayStackDev <raymondlugo64@gmail.com>
2026-04-07 13:27:35 -0400

    fix: Ajustando credenciais de acesso ao MySQL no database e alembic

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:senha@localhost:3306/library_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:11223344@localhost:3306/library_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
