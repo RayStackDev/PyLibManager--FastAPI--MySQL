cc2c97d5bedfb973a17b8e7ab6134a30d61554f4
RayStackDev <raymondlugo64@gmail.com>
2026-04-07 11:37:22 -0400

    feat: implementando modelo Loan com chaves estrangeiras e relacionamentos

--- app/models/loan.py
+++ app/models/loan.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from app.database import Base
 from datetime import datetime
 
-class loan(Base):
+class Loan(Base):
     __tablename__ = "loans"
 
     id = Column(Integer, primary_key=True, index=True)
