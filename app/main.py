8db2817ad0fc451dea25e189943fdcc27dfba7b8
RayStackDev <raymondlugo64@gmail.com>
2026-05-21 12:36:36 -0400

    feat: registrando rotas de emprestimos na instância principal da API

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from app.routers import user_routes, book_routes
+from app.routers import user_routes, book_routes, loan_routes
 
 app = FastAPI(title="PyLibManager")
 
 app.include_router(user_routes.router)
 app.include_router(book_routes.router)
+app.include_router(loan_routes.router)
 
 @app.get("/")
 def read_root():
