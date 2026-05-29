f4adc3dc955ebee4086f9c47e7714c7a850fe916
RayStackDev <raymondlugo64@gmail.com>
2026-05-29 17:02:20 -0400

    feat: criando dependencia get_current_user para validação e extração de tokens JWT

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,4 +1,38 @@
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 from app.database import SessionLocal
+from app.dependencies import get_db
+from app.repositories.user_repository import UserRepository
+from app.security import SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token de acesso inválido ou expirado",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+        
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    user_repo = UserRepository(db)
+    user = user_repo.get_by_id(int(user_id))
+
+    if user is None:
+        raise credentials_exception
+    
+    return user
 
 def get_db():
     db = SessionLocal()
