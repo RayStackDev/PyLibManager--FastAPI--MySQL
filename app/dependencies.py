a0b27f3979dd0b366110208b77cbe501950e0712
RayStackDev <raymondlugo64@gmail.com>
2026-06-16 15:44:47 -0400

    feat: implementando dependencia para proteger rotas restritas

--- app/dependencies.py
+++ app/dependencies.py
@@ -3,6 +3,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
+from app.models.user import User
 from app.repositories.user_repository import UserRepository
 from app.core.security import SECRET_KEY, ALGORITHM
 
@@ -45,3 +46,11 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         raise credentials_exception
     
     return user
+
+def get_current_admin(current_user: User = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Acesso Negado. Esta operação é exclusiva somente ao Administrador"
+        )
+    return current_user
