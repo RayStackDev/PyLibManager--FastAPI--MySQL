cee6d10433b003897000c0b6d2c138ad71137f17
RayStackDev <raymondlugo64@gmail.com>
2026-06-19 11:05:38 -0400

    refactor: ajustando assinatura de return_book e define prazo de 14 dias na criação do emprestimo

--- app/repositories/loan_repository.py
+++ app/repositories/loan_repository.py
@@ -2,17 +2,21 @@ from sqlalchemy.orm import Session
 from app.models.loan import Loan
 from app.models.book import Book
 from app.schemas.loan import LoanCreate
-from datetime import date
+from datetime import date, timedelta
 
 class LoanRepository:
     def __init__(self, db: Session):
         self.db = db
     
     def create_loan(self, loan_data: LoanCreate, user_id: int):
+        today = date.today()
+        data_devolucao_prevista = today + timedelta(days=14)
+
         db_loan = Loan(
             user_id=user_id,
             book_id=loan_data.book_id,
-            loan_date=date.today()
+            loan_date= today,
+            due_date=data_devolucao_prevista
         )
 
         book = self.db.query(Book).filter(Book.id == loan_data.book_id).first()
@@ -27,10 +31,9 @@ class LoanRepository:
     def get_ative_loans(self):
         return self.db.query(Loan).filter(Loan.return_date == None).all()
     
-    def return_book(self, loan_id: int, user_id: int):
+    def return_book(self, loan_id: int):
         db_loan = self.db.query(Loan).filter(
-            Loan.id == loan_id, 
-            Loan.user_id == user_id,
+            Loan.id == loan_id,
             Loan.return_date == None
         ).first()
 
